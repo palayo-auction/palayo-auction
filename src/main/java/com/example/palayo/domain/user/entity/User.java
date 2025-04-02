b76ec41bbbb9c685737ea068d05d90c861bd4f15
songkongji <seungmin576@gmail.com>
2025-04-02 19:06:13 +0900

    feat : 엔티티에 @Table 어노테이션 추가

--- src/main/java/com/example/palayo/domain/user/entity/User.java
+++ src/main/java/com/example/palayo/domain/user/entity/User.java
@@ -2,11 +2,7 @@ package com.example.palayo.domain.user.entity;
 
 import com.example.palayo.common.entity.BaseEntity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Table(name = "users")
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,19 +19,23 @@ public class User extends BaseEntity {
     @Column(nullable = false, unique = true, length = 50)
     private String email;
 
+    @Column(nullable = false, length = 30)
+    private String password;
+
     @Column(nullable = false, unique = true, length = 20)
     private String nickname;
 
     @Column(nullable = false)
     private int pointAmount;
 
-    private User(String email, String nickname, int pointAmount) {
+    private User(String email, String password, String nickname, int pointAmount) {
         this.email = email;
+        this.password = password;
         this.nickname = nickname;
         this.pointAmount = pointAmount;
     }
 
-    public static User of(String email, String nickname, int pointAmount) {
-        return new User(email, nickname, pointAmount);
+    public static User of(String email, String password, String nickname, int pointAmount) {
+        return new User(email, password, nickname, pointAmount);
     }
 }
