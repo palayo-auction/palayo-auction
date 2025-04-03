c8f7ceb44088fc1872bf1d88cd8c87df3d8426f9
myoungminjun <myoungminjun@gmail.com>
2025-04-03 17:38:21 +0900

    refactor : dib Entity @CreatedDate 적용

--- src/main/java/com/example/palayo/config/JwtAuthenticationToken.java
+++ src/main/java/com/example/palayo/config/JwtAuthenticationToken.java
@@ -2,13 +2,16 @@ package com.example.palayo.config;
 
 import com.example.palayo.common.dto.AuthUser;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
 
 public class JwtAuthenticationToken extends AbstractAuthenticationToken {
 
     private final AuthUser authUser;
 
     public JwtAuthenticationToken(AuthUser authUser) {
-        super(authUser.getAuthorities());
+        super(Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
         this.authUser = authUser;
         setAuthenticated(true);
     }
