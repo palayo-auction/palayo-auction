701f561ba98712b6b62461542d452d4a2cc0a1a7
qwerdfJIN <test19980224@gmail.com>
2025-04-03 17:00:29 +0900

    chore: 이전 커밋에 누락된 AuctionStatus enum 추가

--- src/main/java/com/example/palayo/domain/auction/enums/AuctionStatus.java
+++ src/main/java/com/example/palayo/domain/auction/enums/AuctionStatus.java
@@ -1,4 +1,8 @@
 package com.example.palayo.domain.auction.enums;
 
 public enum AuctionStatus {
+	BEFORE_AUCTION, // 경매 시작 전
+	ON_AUCTION, // 경매 진행 중
+	AUCTION_SUCCESS, // 낙찰 완료
+	AUCTION_FAILED // 유찰
 }
