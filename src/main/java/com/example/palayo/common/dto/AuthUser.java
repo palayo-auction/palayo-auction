df96138b83936770bba496fe8aea37b3c158fd17
lamgak12 <tntnqk12@naver.com>
2025-04-03 17:27:18 +0900

    style: image 테이블 이름 변경
    패키지 이름 변경image->itemimage, 클래스 이름 변경 Image->ItemImage, table어노테이션의 테이블 이름 images->item_images

--- src/main/java/com/example/palayo/common/dto/AuthUser.java
+++ src/main/java/com/example/palayo/common/dto/AuthUser.java
@@ -1,25 +1,19 @@
 package com.example.palayo.common.dto;
 
-import com.example.toongallery.domain.user.enums.UserRole;
 import lombok.Getter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Collection;
-import java.util.List;
 
 @Getter
 public class AuthUser {
 
     private final Long userId;
     private final String email;
-    private final UserRole userRole;
-    private final Collection<? extends GrantedAuthority> authorities;
+//    private final UserRole userRole;
+//    private final Collection<? extends GrantedAuthority> authorities;
 
-    public AuthUser(Long userId, String email, UserRole role) {
+    public AuthUser(Long userId, String email/*, UserRole role*/) {
         this.userId = userId;
         this.email = email;
-        this.userRole = role;
-        this.authorities = List.of(new SimpleGrantedAuthority(role.name()));
+//        this.userRole = role;
+//        this.authorities = List.of(new SimpleGrantedAuthority(role.name()));
     }
 }
