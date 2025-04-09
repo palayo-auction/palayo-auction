671aa7149604d131d3ee20bc8689865b1cadfedf
qwerdfJIN <test19980224@gmail.com>
2025-04-10 08:21:10 +0900

    chore: DepositHistory 임시 파일 추가 (dev 최신 반영 시 삭제 예정)

--- src/main/java/com/example/palayo/domain/deposithistory/entity/DepositHistory.java
+++ src/main/java/com/example/palayo/domain/deposithistory/entity/DepositHistory.java
@@ -7,12 +7,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "deposit_histories")
 public class DepositHistory {
 
