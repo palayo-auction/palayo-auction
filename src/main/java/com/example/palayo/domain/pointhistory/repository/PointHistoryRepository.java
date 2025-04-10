1c005d10af0d8dafabd20998807a03787ad73dd6
songkongji <seungmin576@gmail.com>
2025-04-10 13:59:17 +0900

    faet : PointHistories 페이징 검색 추가 이제 유저의 id를 기반으로 포인트 증감을 보는 페이징 검색 기능이 추가됐습니다.

--- src/main/java/com/example/palayo/domain/pointhistory/repository/PointHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.palayo.domain.pointhistory.repository;
-
-import com.example.palayo.domain.pointhistory.entity.PointHistories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PointHistoryRepository extends JpaRepository<PointHistories, Long> {
-}
--- src/main/java/com/example/palayo/domain/pointhistory/service/PointHistoriesService.java
+++ src/main/java/com/example/palayo/domain/pointhistory/service/PointHistoriesService.java
@@ -1,19 +1,26 @@
 package com.example.palayo.domain.pointhistory.service;
 
+import com.example.palayo.domain.pointhistory.dto.PointHistoriesResponse;
 import com.example.palayo.domain.pointhistory.entity.PointHistories;
-import com.example.palayo.domain.pointhistory.repository.PointHistoryRepository;
+import com.example.palayo.domain.pointhistory.repository.PointHistoriesRepository;
 import com.example.palayo.domain.user.enums.PointType;
 import com.example.palayo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class PointService {
+public class PointHistoriesService {
 
     private final UserRepository userRepository;
-    private final PointHistoryRepository pointHistoryRepository;
+    private final PointHistoriesRepository pointHistoriesRepository;
 
     @Transactional
     public void chargePoints(Long userId, int amount) {
@@ -27,8 +34,15 @@ public class PointService {
                     .pointType(PointType.RECHARGE)
                     .build();
 
-            pointHistoryRepository.save(history);
+            pointHistoriesRepository.save(history);
         });
+    }
+
+    public Page<PointHistoriesResponse> findByUserId(Long userId, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.by("createdAt").descending());
+        Page<PointHistories> pointHistories = pointHistoriesRepository.findByUserId(userId, pageable);
+
+        return pointHistories.map(PointHistoriesResponse::of);
     }
 }
 
