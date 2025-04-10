0c980cc49d2e68d2557d282a6894969dd3d9901b
songkongji <seungmin576@gmail.com>
2025-04-10 14:04:20 +0900

    style : PaymentConfirmRequest 의 @AllArgConstructor 삭제 미사용으로 인한 삭제

--- src/main/java/com/example/palayo/domain/payment/dto/request/PaymentConfirmRequest.java
+++ src/main/java/com/example/palayo/domain/payment/dto/request/PaymentConfirmRequest.java
@@ -1,10 +1,8 @@
 package com.example.palayo.domain.payment.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class PaymentConfirmRequest {
     private String paymentKey;
     private String orderId;
