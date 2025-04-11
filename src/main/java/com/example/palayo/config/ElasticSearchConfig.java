7ae00a814140c01861302b912ed42411f90a008e
songkongji <seungmin576@gmail.com>
2025-04-11 11:29:09 +0900

    feat : 엘라스틱서치 사용하기 위한 기초 세팅 의존성 추가, 도커컴포즈 파일 추가, 컨피그 클래스가 추가됐습니다. 아직 세팅 파일만 추가된 것이며 사용은 불가능합니다.

---
+++ src/main/java/com/example/palayo/config/ElasticSearchConfig.java
@@ -0,0 +1,21 @@
+package com.example.palayo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+    @Value("${spring.elasticsearch.uris}")
+    private String host;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(host)
+                .build();
+    }
+}
