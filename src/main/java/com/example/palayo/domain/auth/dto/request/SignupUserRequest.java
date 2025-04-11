84db29b9b439aba62a80b9633cc2e30ff372c618
dlwjdals3667@gmail.com <dlwjdals3667@gmail.com>
2025-04-11 19:14:03 +0900

    fix:코드 리뷰 적용

--- src/main/java/com/example/palayo/domain/auth/dto/request/SignupUserRequest.java
+++ src/main/java/com/example/palayo/domain/auth/dto/request/SignupUserRequest.java
@@ -3,13 +3,14 @@ package com.example.palayo.domain.auth.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SignupUserRequest {
 
     @Email(message = "이메일 형식에 어긋납니다.")
