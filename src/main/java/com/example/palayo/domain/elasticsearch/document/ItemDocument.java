f766a699686948af2b84eb5b3f230be527824c51
songkongji <seungmin576@gmail.com>
2025-04-11 15:41:29 +0900

    feat : json 파일 작성 및 ItemDocument 변경 인덱스 매핑과 세팅에 필요한 정보를 적은 json 파일이 작성완료 됐으며, ItemDocument에는 검색에만 필요한 필드만 남기고 응답에만 필요한 필드는 제거했음.

--- src/main/java/com/example/palayo/domain/elasticsearch/document/ItemDocument.java
+++ src/main/java/com/example/palayo/domain/elasticsearch/document/ItemDocument.java
@@ -21,31 +21,20 @@ public class ItemDocument {
     @Id
     private Long id;
 
-    @Field(type = FieldType.Text)
     private String name;
 
-    @Field(type = FieldType.Text)
     private String content;
 
-    @Field(type = FieldType.Keyword)
     private Category category;
 
-    @Field(type = FieldType.Keyword)
-    private ItemStatus itemStatus;
-
-    @Field(type = FieldType.Text)
     private Long sellerId;
 
-    @Field(type = FieldType.Nested)
-    private List<ItemImage> itemImages = new ArrayList<>();
-
-    public static ItemDocument of(Long id, String name, String content, Category category, ItemStatus itemStatus, Long sellerId) {
+    public static ItemDocument of(Long id, String name, String content, Category category, Long sellerId) {
         ItemDocument item = new ItemDocument();
         item.id = id;
         item.name = name;
         item.content = content;
         item.category = category;
-        item.itemStatus = itemStatus;
         item.sellerId = sellerId;
         return item;
     }
