35763c96943db9ee085a9f00d476a79330abd076
songkongji <seungmin576@gmail.com>
2025-04-14 13:52:38 +0900

    feat : index 매핑 변경, document 클래스의 필드 추가, ElasticSearchConfig 변경 도큐먼트 클래스에 응답에만 필요한 필드인 itemStatus 라는 필드가 추가됐습니다.  그에 따라 index 매핑 파일에서 itemStatus는 index : false 처리를 했습니다.
     컨피그 파일이 더 이상 ElasticSearchConfiguration을 상속받지 않고 빈으로 등록하는 형식으로 바뀌었습니다.
     서비스 코드에서 ElasticsearchClient을 사용하기 위해 직접 빈으로 등록하는 방식으로 변경됐습니다.
     최신 버전의 엘라스틱서치와 스프링부트에서는 elasticsearch rest high level client가 deprecated 됐기 때문입니다.

--- src/main/java/com/example/palayo/config/ElasticSearchConfig.java
+++ src/main/java/com/example/palayo/config/ElasticSearchConfig.java
@@ -1,21 +1,33 @@
 package com.example.palayo.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories
-public class ElasticSearchConfig extends ElasticsearchConfiguration {
+public class ElasticSearchConfig {
     @Value("${spring.elasticsearch.uris}")
     private String host;
 
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(host)
-                .build();
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder(HttpHost.create(host)).build();
+    }
+
+    @Bean
+    public RestClientTransport transport() {
+        return new RestClientTransport(restClient(), new JacksonJsonpMapper());
+    }
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        return new ElasticsearchClient(transport());
     }
 }
