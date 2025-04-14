555b27fbf8b74cbfdcadb0ed11efebd8bbb75098
songkongji <seungmin576@gmail.com>
2025-04-14 20:24:48 +0900

    style : controller의 주소 앞에 "/" 추가 까먹고 안쓴 곳들에 추가

--- src/main/java/com/example/palayo/domain/elasticsearch/controller/ItemSearchController.java
+++ src/main/java/com/example/palayo/domain/elasticsearch/controller/ItemSearchController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api")
+@RequestMapping("/api")
 public class ItemSearchController {
 
     private final ItemSearchService itemSearchService;
