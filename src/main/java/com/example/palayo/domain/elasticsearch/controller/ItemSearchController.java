8e85286c66fa8d0a9fed802c5109ef171b5d1dbb
songkongji <seungmin576@gmail.com>
2025-04-14 13:59:05 +0900

    feat : 검색 기능을 구현했습니다. v1의 경우 repository 사용 방식이 아닌 elasticsearchClient와 QueyDSL을 사용하여 동적 쿼리를 직접작성하는 방식의 검색기능입니다. sellerId를 기반으로 검색하되, keyword와 category를 검색 조건에 추가 가능합니다.
     keyword를 추가할 경우 :
     keyword가 name, content와 multiMatch가 되어 상품명 검색, 또는 상품 내역 검색이 가능합니다.
    
     category를 추가할 경우 :
     카테고리 검색이 가능합니다.
    
     즉 keyword와 category를 추가하면 검색 내역이 좁아집니다.

---
+++ src/main/java/com/example/palayo/domain/elasticsearch/controller/ItemSearchController.java
@@ -0,0 +1,46 @@
+package com.example.palayo.domain.elasticsearch.controller;
+
+import com.example.palayo.common.dto.AuthUser;
+import com.example.palayo.common.response.Response;
+import com.example.palayo.domain.elasticsearch.dto.response.ItemSearchResponse;
+import com.example.palayo.domain.elasticsearch.service.ItemSearchService;
+import com.example.palayo.domain.item.enums.Category;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api")
+public class ItemSearchController {
+
+    private final ItemSearchService itemSearchService;
+
+    @GetMapping("/v1/items/search")
+    public Response<List<ItemSearchResponse>> searchItems(
+            @RequestParam String keyword,
+            @RequestParam(required = false) Category category,
+            @AuthenticationPrincipal AuthUser authUser,
+            Pageable pageable
+            )  {
+        Page<ItemSearchResponse> responses = itemSearchService.searchItems(keyword, category, authUser.getUserId(), pageable);
+        return Response.fromPage(responses);
+    }
+
+    @GetMapping("/v2/items/search")
+    public Response<List<ItemSearchResponse>> findBySellerId(
+            @AuthenticationPrincipal AuthUser authUser,
+            Pageable pageable
+    ) {
+        Page<ItemSearchResponse> responses = itemSearchService.findBySellerId(authUser.getUserId(), pageable);
+        return Response.fromPage(responses);
+    }
+}
