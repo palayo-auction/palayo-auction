9ff135bfbeef82226d95392df52fca2ee7bfc222
songkongji <seungmin576@gamil.com>
2025-04-18 00:05:26 +0900

    revert : 잘못된 cd 구축 시도로 인해 되돌리기

--- src/main/java/com/example/palayo/domain/payment/repostiory/PaymentRepository.java
+++ src/main/java/com/example/palayo/domain/payment/repostiory/PaymentRepository.java
@@ -1,10 +1,7 @@
 package com.example.palayo.domain.payment.repostiory;
 
 import com.example.palayo.domain.payment.entity.Payment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Page<Payment> findByUserId(Long userId, Pageable pageable);
 }
