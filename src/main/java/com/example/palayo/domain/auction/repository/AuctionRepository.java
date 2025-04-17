01bc216ebccb7c5ea63ae33ade7426f04453d4cd
songkongji <seungmin576@gamil.com>
2025-04-17 16:19:43 +0900

    style : 사용하지 않는 import 삭제

--- src/main/java/com/example/palayo/domain/auction/repository/AuctionRepository.java
+++ src/main/java/com/example/palayo/domain/auction/repository/AuctionRepository.java
@@ -3,7 +3,6 @@ package com.example.palayo.domain.auction.repository;
 import java.util.List;
 import java.util.Optional;
 
-import com.example.palayo.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
