af2731fde7f803434fbc1486a14df7439b7dfdcc
lamgak12 <tntnqk12@naver.com>
2025-04-17 10:32:56 +0900

    refactor: itemImage도메인 리팩토링

--- src/main/java/com/example/palayo/domain/itemimage/controller/ItemImageController.java
+++ src/main/java/com/example/palayo/domain/itemimage/controller/ItemImageController.java
@@ -1,5 +1,6 @@
 package com.example.palayo.domain.itemimage.controller;
 
+import com.example.palayo.common.dto.AuthUser;
 import com.example.palayo.common.response.Response;
 import com.example.palayo.domain.itemimage.dto.request.UpdateImageUrlRequest;
 import com.example.palayo.domain.itemimage.dto.request.CreateItemImageRequest;
@@ -7,6 +8,7 @@ import com.example.palayo.domain.itemimage.dto.request.UpdateItemImageRequest;
 import com.example.palayo.domain.itemimage.dto.response.ItemImageResponse;
 import com.example.palayo.domain.itemimage.service.ItemImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,17 +21,19 @@ public class ItemImageController {
 
     @PostMapping("/v1/items/{itemId}/images")
     public Response<List<ItemImageResponse>> uploadImageInfo(
+            @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long itemId,
             @RequestBody List<CreateItemImageRequest> request) {
-        List<ItemImageResponse> itemImageResponses = itemImageService.saveImages(itemId, request);
+        List<ItemImageResponse> itemImageResponses = itemImageService.saveImages(authUser.getUserId(), itemId, request);
         return Response.of(itemImageResponses);
     }
 
     @PatchMapping("/v1/items/{itemId}/images")
     public Response<List<ItemImageResponse>> updateImageInfo(
+            @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long itemId,
             @RequestBody List<UpdateItemImageRequest> request) {
-        List<ItemImageResponse> itemImageResponses = itemImageService.updateImageInfo(itemId, request);
+        List<ItemImageResponse> itemImageResponses = itemImageService.updateImageInfo(authUser.getUserId(), itemId, request);
         return Response.of(itemImageResponses);
     }
 
@@ -43,10 +47,11 @@ public class ItemImageController {
 
     @PatchMapping("/v1/items/{itemId}/urls")
     public Response<List<ItemImageResponse>> updateImageFiles(
+            @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long itemId,
             @RequestBody List<UpdateImageUrlRequest> request
     ) {
-        List<ItemImageResponse> updated = itemImageService.updateImageUrl(itemId, request);
+        List<ItemImageResponse> updated = itemImageService.updateImageUrl(authUser.getUserId(), itemId, request);
         return Response.of(updated);
     }
 }
