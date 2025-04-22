eb6e5f84670d0d9743c2270e31cb890e5dc075de
songkongji <seungmin576@gamil.com>
2025-04-22 21:53:57 +0900

    fix : firebase 관련 json파일의 경로를 ec2에서 못찾는 문제 때문에 수정

--- src/main/java/com/example/palayo/config/FirebaseConfig.java
+++ src/main/java/com/example/palayo/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
@@ -12,10 +13,12 @@ import java.io.FileInputStream;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.config-path}")
+    private String firebaseConfigPath;
+
     @PostConstruct
     public void initFirebase() throws Exception {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/service_account_key.json");
+        FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
