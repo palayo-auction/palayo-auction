e50336fb71f6363eb5d4f93b47b9c4ca8a7595be
david <bsj9254@gmail.com>
2025-04-24 15:23:39 +0900

    yml, app.java 원복

--- src/main/java/com/example/palayo/PalayoApplication.java
+++ src/main/java/com/example/palayo/PalayoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.boot.WebApplicationType;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -15,14 +14,7 @@ public class PalayoApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication app = new SpringApplication(PalayoApplication.class);
-		System.out.println(" PalayoApplication started ");
-		app.setWebApplicationType(WebApplicationType.SERVLET);
-		app.run(args);
-		System.out.println(" PalayoApplication after started ");
-
-
-
+		SpringApplication.run(PalayoApplication.class, args);
 	}
 
 }
