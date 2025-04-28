9c569d2ff02c553d117bb69f469314135fbbddec
songkongji <seungmin576@gamil.com>
2025-04-28 11:13:16 +0900

    feat : 실시간 입찰내역 갱신 기능 개발 이제 입찰시 실시간으로 새로고침없이 입찰내역이 갱신됩니다. 포함하고 있는 정보는 유저 닉네임, 옥션ID, 입찰가격, 현재 날짜와 시간입니다.

--- src/main/java/com/example/palayo/domain/websocket/controller/BidWebSocketController.java
+++ src/main/java/com/example/palayo/domain/websocket/controller/BidWebSocketController.java
@@ -41,6 +41,7 @@ public class BidWebSocketController {
         }
 
         log.info("Authenticated User: {}", authUser);
+        log.info("nickname@@@@!@#@!#@!!@#!@#@!##!@#!@!@#!@#@!##@!@: {}", authUser.getNickname());
 
         BidResponse response;
         try {
