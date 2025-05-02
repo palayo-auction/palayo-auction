36b9e66044a456747d20a8c89ec3474a103c59e6
lamgak12 <tntnqk12@naver.com>
2025-05-03 04:00:59 +0900

    test용 yml수정

--- src/test/java/com/example/palayo/PalayoApplicationTests.java
+++ src/test/java/com/example/palayo/PalayoApplicationTests.java
@@ -2,9 +2,11 @@ package com.example.palayo;
 
 import com.example.palayo.common.util.S3Uploader;
 import com.example.palayo.domain.elasticsearch.repository.ItemElasticSearchRepository;
+import com.example.palayo.domain.notification.scheduler.RedisNotificationScheduler;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
@@ -15,6 +17,12 @@ class PalayoApplicationTests {
 
    @MockitoBean
    private S3Uploader s3Uploader;
+
+   @MockitoBean
+   private RedissonClient redissonClient;
+
+   @MockitoBean
+   private RedisNotificationScheduler redisNotificationScheduler;
 
    @BeforeAll
    static void loadEnv() {
